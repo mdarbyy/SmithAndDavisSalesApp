91b97c7031da8150b026e9abd1a93d654e214ba3
mdarbyy <michaeldarby2698@gmail.com>
2025-11-12 14:29:16 -0500

    Built out Dashboard
    built out the dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,14 @@ class StaticPagesController < ApplicationController
 
     @start_date = Date.strptime(params[:start_date], '%m/%d/%Y')
     @end_date = Date.strptime(params[:end_date], '%m/%d/%Y')
+
+    @sales_records = SalesRecord.where(sell_date: @start_date..@end_date)
+    if @sales_records.count == 0
+      redirect_to request.referrer, danger: "No sales were recorded within this date range"
+      return
+    end
+
+    sales_person_ids = @sales_records.pluck(:sales_person_id).uniq
+    @sales_people = SalesPerson.where(id: sales_person_ids)
   end
 end
