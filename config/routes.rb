c4ee33baa46453d19c18759b9e8cc9946e0eff1a
mdarbyy <michaeldarby2698@gmail.com>
2025-12-03 19:10:25 -0500

    Removed unused route
    removed a route that is unused in the app

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :sales_records
   resources :sales_people
   get 'static_pages/dashboard', to: 'static_pages#dashboard', as: 'dashboard'
-  post "/create_sales_record", to: "sales_records#create_sales_record"
   root "static_pages#dashboard"
 end
