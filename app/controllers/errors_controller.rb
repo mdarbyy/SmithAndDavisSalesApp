4dfb4612d6b64ae27c7abb5cf35adca96fa507b1
mdarbyy <michaeldarby2698@gmail.com>
2025-12-11 20:14:55 -0500

    Updated errors controller
    updated the errors controller to use a variable to reference the message in two places

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,10 +3,11 @@ class ErrorsController < ApplicationController
   skip_before_action :check_authentication
 
   def not_found
+    error_message = "That page does not exist"
     if user_signed_in?
-      redirect_to dashboard_path, danger: "That page does not exist"
+      redirect_to dashboard_path, danger: error_message
     else
-      redirect_to new_user_session_path, danger: "That page does not exist"
+      redirect_to new_user_session_path, danger: error_message
     end
   end
 end
