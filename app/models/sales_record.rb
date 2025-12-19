0fec0e476a87878c9e90fc70df2d7793827f89d5
mdarbyy <michaeldarby2698@gmail.com>
2025-12-18 22:30:23 -0500

    Fixed decimal places issue
    fixed an issue with creating/updated sales records and in the dashboard where the sales floor hours and project hours could be any number of decimal places

--- app/models/sales_record.rb
+++ app/models/sales_record.rb
@@ -1,29 +1,24 @@
 class SalesRecord < ApplicationRecord
   belongs_to :sales_person
   validates :sell_date, presence: true
-  before_create :ensure_unique_sales_record
-  before_create :ensure_fields_filled
-  before_update :ensure_unique_sales_record
-  before_update :ensure_fields_filled
+
+  before_save :ensure_fields_filled
+  before_save :ensure_unique_sales_record
 
   private
 
   def ensure_fields_filled
-    self.amount_sold       ||= 0
-    self.sales_floor_hours ||= 0
-    self.project_hours     ||= 0
+    self.amount_sold       = (amount_sold || 0).round(2)
+    self.sales_floor_hours = (sales_floor_hours || 0).round(2)
+    self.project_hours     = (project_hours || 0).round(2)
   end
 
   def ensure_unique_sales_record
-    
-    duplicate = if persisted?
-      SalesRecord.where(sales_person_id: sales_person_id, sell_date: sell_date)
+    duplicate = SalesRecord
+      .where(sales_person_id: sales_person_id, sell_date: sell_date)
       .where.not(id: id)
       .exists?
-    else
-      SalesRecord.exists?(sales_person_id: sales_person_id, sell_date: sell_date)
-    end
-    
+
     if duplicate
       errors.add(:base, "A Sales Record for this Sales Person already exists on that date")
       throw(:abort)
